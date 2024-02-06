bda18bb0be38feb1cbf8c09e49057b519735b958
Seth Brock <sab716@byu.edu>
2024-02-05 18:32:37 -0700

    Many updates, including tutoring calculator

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,10 @@ namespace Mission5WebApp.Controllers {
             return View();
         }
 
+        public IActionResult Tutoring() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿// Calculator function to find out the estimated cost for soccer tutoring
+$('#calculate-cost-btn').click(function () {
+    // If the value entered is a number that is above or equal to tutoring, show the cost. If not, ask for valid input
+    if ($.isNumeric($('#hours-input').val()) && ($('#hours-input').val() >= 0)) {
+        var cost = ($('#hours-input').val() * 20);
+        $('#cost-output').html('$' + cost + ' is your estimated cost for ' + $('#hours-input').val() +
+            ($('#hours-input').val() == 1 ? ' hour' : ' hours') + ' of soccer tutoring.');
+    }
+    else { $('#cost-output').html('Please enter a valid positive number'); }
+});
