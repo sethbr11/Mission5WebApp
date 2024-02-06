95efacbc9aff52896d4296bbdd40e7ea5ac6d29a
Seth Brock <sab716@byu.edu>
2024-02-06 08:08:40 -0700

    Rounding for cost estimation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,7 @@
 $('#calculate-cost-btn').click(function () {
     // If the value entered is a number that is above or equal to tutoring, show the cost. If not, ask for valid input
     if ($.isNumeric($('#hours-input').val()) && ($('#hours-input').val() >= 0)) {
-        var cost = ($('#hours-input').val() * 20);
+        var cost = Math.round(($('#hours-input').val() * 20));
         $('#cost-output').html('$' + cost + ' is your estimated cost for ' + $('#hours-input').val() +
             ($('#hours-input').val() == 1 ? ' hour' : ' hours') + ' of soccer tutoring.');
     }
